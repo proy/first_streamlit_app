3bd4925d400e3275888ccf523ab17acb151b8c8b
proy <proy71@gmail.com>
2023-07-27 03:15:38 +0530

    Update streamlit_app.py
    Added emojis and a new header and an item in Breakfast favs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
 
 streamlit.title("My parents New Healthy Diner")
+   
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spinach and Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-range Egg")
+streamlit.header("Breakfast Favourites")
+streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-range Egg")
+streamlit.text("🥑🍞 Avacado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
