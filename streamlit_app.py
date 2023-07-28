3bc2ef66b06b6c47c67fae981539e6f07c28cfbd
proy <proy71@gmail.com>
2023-07-29 01:01:16 +0530

    update json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import json
 streamlit.title("My parents New Healthy Diner")
    
 
@@ -24,6 +25,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
