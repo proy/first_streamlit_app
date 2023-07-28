159f164a8a885c27aea575f259fc5a38b61f65ca
proy <proy71@gmail.com>
2023-07-29 03:35:16 +0530

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,14 +44,14 @@ except URLError as e:
 
 
 streamlit.header("The fruit load list contain:")
-def get_fruit_load_list():
+def get_fruit_load_list(my_cnx):
     with my_cnx.cursor as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
 if streamlit.button('Get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
+    my_data_rows = get_fruit_load_list(my_cnx)
     streamlit.dataframe(my_data_rows)
 
 streamlit.stop()
