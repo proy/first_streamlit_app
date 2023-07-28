8de04c5cc686ab8c791b39ba577ad16e649e3350
proy <proy71@gmail.com>
2023-07-29 02:46:53 +0530

    adsfasdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like ito add ?','coconut')
 streamlit.write('The user entered ', add_my_fruit)
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('streamlit')")
