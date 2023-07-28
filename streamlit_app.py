3e2ea439d9ae62537143d7159d784e41828d53d0
proy <proy71@gmail.com>
2023-07-29 03:29:43 +0530

    sdfas

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,12 +54,7 @@ if streamlit.button('Get fruit load list'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-
-
-
-
-streamlit.dataframe(my_data_rows)
-
+streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like ito add ?','coconut')
 streamlit.write('The user entered ', add_my_fruit)
 my_cur.execute("insert into FRUIT_LOAD_LIST values (' from streamlit')")
